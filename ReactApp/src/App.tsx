40ae571ff6eb27eadf67a2c029989c40c63b98a8
e-a-11 <apollo_estrella@yahoo.com>
2025-05-22 18:59:49 +0800

    Create new React Application project.

---
+++ ReactApp/src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import React from 'react';
+import ProductList from './components/ProductList';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <ProductList />
+        </div>
+    );
+};
+
+export default App;
